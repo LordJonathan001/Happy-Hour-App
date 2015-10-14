79c27bff5f2ab94200562386b15e45b49dff4e56
LordJonathan001 <linglejs13@mounties.mansfield.edu>
2015-10-14 17:21:37 -0400

    Working Map
    I did dat shit

---
+++ MapApp(New)/src/com/example/craigslistitemfinder/NoResults.java
@@ -0,0 +1,30 @@
+package com.example.craigslistitemfinder;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.support.v4.app.Fragment;
+
+public class NoResults extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_no_results);
+		Button b = (Button)findViewById(R.id.btnGPS);
+		Button c = (Button)findViewById(R.id.btnNewSearch);
+		
+		b.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				startActivity(new Intent(NoResults.this, MainActivity.class));
+				
+			};
+		});
+	}
+}
