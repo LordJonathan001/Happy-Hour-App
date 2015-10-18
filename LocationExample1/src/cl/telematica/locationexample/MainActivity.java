b211a3f7c0071ce330d67d7ce7c6b5cb323aab6b
dajanra <vancej10@mounties.mansfield.edu>
2015-10-17 22:48:56 -0400

    Working Locations
    Cities and their GPS is coded. Pings user GPS and stores that, as well
    as lets you select a different city to search and stores that GPS. GPS
    locations displaying for debugging purposes

--- LocationExample1/src/cl/telematica/locationexample/MainActivity.java
+++ LocationExample1/src/cl/telematica/locationexample/MainActivity.java
@@ -11,170 +11,178 @@ import android.view.Menu;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends ActiveLocationManagerActivity {
 	
-	public static String citylatitudText;
-	public static String citylongitudText;
-	public static TextView latitudText;
-	public static TextView longitudText;
-	public String locationSet;
+	public static String citylatitudeText;
+	public static String citylongitudeText;
+	public static TextView latitudeText;
+	public static TextView longitudeText;
+	public static String locationSet;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+		
 		Button b = (Button)findViewById(R.id.btnTestNoResults);
 		Button c = (Button)findViewById(R.id.btnGPSSearch);
 		Button d = (Button)findViewById(R.id.btnSetCity);
-		Spinner spinner = (Spinner)findViewById(R.id.spinner1);
-		final String locationSet = spinner.getSelectedItem().toString();
+				
+		final Spinner spinner = (Spinner)findViewById(R.id.spinner1);
+		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+		dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+				 
 		b.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
 				startActivity(new Intent(MainActivity.this, NoResults.class));
 			};
 		});
 		c.setOnClickListener(new OnClickListener() {
 			
 			@Override
-			public void onClick(View v) {
-				//if ()
-				// TODO Auto-generated method stub
+			public void onClick(View v) {			
 				startActivity(new Intent(MainActivity.this, NewSearch.class));
 			};
 		});
 		
 		d.setOnClickListener(new OnClickListener() {
 			
+			 
 			@Override
 			public void onClick(View v) {
-				if( locationSet == "Washington D.C.")
-				{ citylatitudText = "38.9047 N";
-				citylongitudText = "77.0164 W";
+				
+				String locationSet = String.valueOf(spinner.getSelectedItem());
+				if( locationSet.equals("Washington D.C.") )
+				{ citylatitudeText = "38.9047";
+				citylongitudeText = "-77.0164";
 				}
-				else if (locationSet == "Mansfield, PA")
-				{citylatitudText = "41.8061 N";
-				citylongitudText = "77.0756 W";			
+				else if (locationSet.equals("Mansfield, PA"))
+				{citylatitudeText = "41.8061";
+				citylongitudeText = "-77.0756";			
 				}
-				else if (locationSet == "New York, NY")
-				{citylatitudText = "40.7127° N";
-				citylongitudText = "74.0059° W";			
+				else if (locationSet.equals( "New York, NY"))
+				{citylatitudeText = "40.7127";
+				citylongitudeText = "-74.0059";			
 				}
-				else if (locationSet == "Juneau, AL")
-				{citylatitudText = "58.3000° N";
-				citylongitudText = "134.4167° W";			
+				else if (locationSet.equals( "Juneau, AL"))
+				{citylatitudeText = "58.3000";
+				citylongitudeText = "-134.4167";			
 				}
-				else if (locationSet == "Phoenix, AZ")
-				{citylatitudText = "33.4500° N";
-				citylongitudText = "112.0667° W";			
+				else if (locationSet.equals("Phoenix, AZ"))
+				{citylatitudeText = "33.4500";
+				citylongitudeText = "-112.0667";			
 				}
-				else if (locationSet == "Tucson, AZ")
-				{citylatitudText = "32.2217° N";
-				citylongitudText = "110.9264° W";			
+				else if (locationSet.equals("Tucson, AZ"))
+				{citylatitudeText = "32.2217";
+				citylongitudeText = "-110.9264";			
 				}
-				else if (locationSet == "Sacramento, CA")
-				{citylatitudText = "38.5556° N";
-				citylongitudText = "121.4689° W";			
+				else if (locationSet.equals( "Sacramento, CA"))
+				{citylatitudeText = "38.5556";
+				citylongitudeText = "-121.4689";			
 				}
-				else if (locationSet == "San Diego, CA")
-				{citylatitudText = "32.7150° N";
-				citylongitudText = "117.1625° W";			
+				else if (locationSet.equals("San Diego, CA"))
+				{citylatitudeText = "32.7150";
+				citylongitudeText = "-117.1625";			
 				}
-				else if (locationSet == "Los Angeles, CA")
-				{citylatitudText = "34.0500° N";
-				citylongitudText = "118.2500° W";			
+				else if (locationSet.equals("Los Angeles, CA"))
+				{citylatitudeText = "34.0500";
+				citylongitudeText = "-118.2500";			
 				}
-				else if (locationSet == "Denver, CO")
-				{citylatitudText = "39.7392° N";
-				citylongitudText = "104.9903° W";			
+				else if (locationSet.equals("Denver, CO"))
+				{citylatitudeText = "39.7392";
+				citylongitudeText = "-104.9903";			
 				}
-				else if (locationSet == "Jacksonville, FL")
-				{citylatitudText = "30.3369° N";
-				citylongitudText = "81.6614° W";			
+				else if (locationSet.equals("Jacksonville, FL"))
+				{citylatitudeText = "30.3369";
+				citylongitudeText = "-81.6614";			
 				}
-				else if (locationSet == "Orlando, FL")
-				{citylatitudText = "28.4158° N";
-				citylongitudText = "81.2989° W";			
+				else if (locationSet.equals("Orlando, FL"))
+				{citylatitudeText = "28.4158";
+				citylongitudeText = "-81.2989";			
 				}
-				else if (locationSet == "Atlanta, GA")
-				{citylatitudText = "33.7550° N";
-				citylongitudText = "84.3900° W";			
+				else if (locationSet.equals("Atlanta, GA"))
+				{citylatitudeText = "33.7550";
+				citylongitudeText = "-84.3900";			
 				}
-				else if (locationSet == "Boise, ID")
-				{citylatitudText = "43.6167° N";
-				citylongitudText = "116.2000° W";			
+				else if (locationSet.equals("Boise, ID"))
+				{citylatitudeText = "43.6167";
+				citylongitudeText = "-116.2000";			
 				}
-				else if (locationSet == "Chicago, IL")
-				{citylatitudText = "41.8369° N";
-				citylongitudText = "87.6847° W";			
+				else if (locationSet.equals("Chicago, IL"))
+				{citylatitudeText = "41.8369";
+				citylongitudeText = "-87.6847";			
 				}
-				else if (locationSet == "Indianapolis, IN")
-				{citylatitudText = "39.7910° N";
-				citylongitudText = "86.1480° W";			
+				else if (locationSet.equals("Indianapolis, IN"))
+				{citylatitudeText = "39.7910";
+				citylongitudeText = "-86.1480";			
 				}
-				else if (locationSet == "Des Moines, IA")
-				{citylatitudText = "41.5908° N";
-				citylongitudText = "93.6208° W";			
+				else if (locationSet.equals("Des Moines, IA"))
+				{citylatitudeText = "41.5908";
+				citylongitudeText = "-93.6208";			
 				}
-				else if (locationSet == "Kansas City, KS")
-				{citylatitudText = "39.1067° N";
-				citylongitudText = "94.6764° W";			
+				else if (locationSet.equals("Kansas City, KS"))
+				{citylatitudeText = "39.1067";
+				citylongitudeText = "-94.6764";			
 				}
-				else if (locationSet == "Louisville, KY")
-				{citylatitudText = "38.2500° N";
-				citylongitudText = "85.7667° W";			
+				else if (locationSet.equals("Louisville, KY"))
+				{citylatitudeText = "38.2500";
+				citylongitudeText = "-85.7667";			
 				}
-				else if (locationSet == "New Orleans, LA")
-				{citylatitudText = "29.9500° N";
-				citylongitudText = "90.0667° W";			
+				else if (locationSet.equals("New Orleans, LA"))
+				{citylatitudeText = "29.9500";
+				citylongitudeText = "-90.0667";			
 				}
-				else if (locationSet == "Portland, ME")
-				{citylatitudText = "43.6667° N";
-				citylongitudText = "70.2667° W";			
+				else if (locationSet.equals("Portland, ME"))
+				{citylatitudeText = "43.6667";
+				citylongitudeText = "-70.2667";			
 				}
-				else if (locationSet == "Baltimore, MD")
-				{citylatitudText = "39.2833° N";
-				citylongitudText = "76.6167° W";			
+				else if (locationSet.equals("Baltimore, MD"))
+				{citylatitudeText = "39.2833";
+				citylongitudeText = "-76.6167";			
 				}
-				else if (locationSet == "Boston, MA")
-				{citylatitudText = "42.3601° N";
-				citylongitudText = "71.0589° W";			
+				else if (locationSet.equals("Boston, MA"))
+				{citylatitudeText = "42.3601";
+				citylongitudeText = "-71.0589";			
 				}
-				else if (locationSet == "Detroit, MI")
-				{citylatitudText = "42.3314° N";
-				citylongitudText = "83.0458° W";			
+				else if (locationSet.equals("Detroit, MI"))
+				{citylatitudeText = "42.3314";
+				citylongitudeText = "-83.0458";			
 				}
-				else if (locationSet == "Minneapolis, MN")
-				{citylatitudText = "44.9778° N";
-				citylongitudText = "93.2650° W";			
+				else if (locationSet.equals("Minneapolis, MN"))
+				{citylatitudeText = "44.9778";
+				citylongitudeText = "-93.2650";			
 				}
-				else if (locationSet == "Seattle, WA")
-				{citylatitudText = "47.6097° N";
-				citylongitudText = "122.3331° W";			
+				else if (locationSet.equals("Seattle, WA"))
+				{citylatitudeText = "47.6097";
+				citylongitudeText = "-122.3331";			
 				}
 				else
-				{citylatitudText = "39.9500° N";
-				citylongitudText = "75.1667° W";			
+				{citylatitudeText = "39.9500";
+				citylongitudeText = "-75.1667";			
 				}				
 				// TODO Auto-generated method stub
 				startActivity(new Intent(MainActivity.this, NewSearch.class));
 			};
 		});
-		latitudText = (TextView) findViewById(R.id.textView4);
-		longitudText = (TextView) findViewById(R.id.textView5);
+		
+		latitudeText = (TextView) findViewById(R.id.cityLong);
+		longitudeText = (TextView) findViewById(R.id.cityLat);
 		
 		listener = new LocationListenerHandler() {
 			@Override
 			public void OnLocationReceived(Location loc) {
-				latitudText.setText("" + loc.getLatitude());
-				longitudText.setText("" + loc.getLongitude());
+				latitudeText.setText("" + loc.getLatitude());
+				longitudeText.setText("" + loc.getLongitude());
 			}
         };
 	}
